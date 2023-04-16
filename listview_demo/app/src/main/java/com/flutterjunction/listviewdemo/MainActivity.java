65b836b834451b542ef4a7cddb7a929f82e5bf60
Nabin <ndhakal10@gmail.com>
2023-04-16 22:51:58 +0545

    Initial Commit

---
+++ listview_demo/app/src/main/java/com/flutterjunction/listviewdemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.flutterjunction.listviewdemo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list_view);
+
+        String[] items = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item, R.id.list_item_text, items);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, "You clicked " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
