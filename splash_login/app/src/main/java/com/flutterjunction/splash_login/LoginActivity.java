62a36a9dd090ce14244be2e2663f859a9fbe333e
Nabin <ndhakal10@gmail.com>
2023-04-26 23:02:51 +0545

    Initial Commit

---
+++ splash_login/app/src/main/java/com/flutterjunction/splash_login/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.flutterjunction.splash_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText usernameEditText, passwordEditText;
+    private Button loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                Toast.makeText(LoginActivity.this,"Username is "+username+"Password is"+password,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ splash_login/app/src/main/java/com/flutterjunction/splash_login/MainActivity.java
@@ -0,0 +1,31 @@
+package com.flutterjunction.splash_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIMEOUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //To hide AppBar
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
