c7eb4982782489e22598e808014e9040ec07537b
Nabin <ndhakal10@gmail.com>
2023-05-02 21:15:30 +0545

    Life cycle

--- activity_life_cycle/app/src/main/java/com/flutterjunction/activitylifecycle/MainActivity.java
+++ activity_life_cycle/app/src/main/java/com/flutterjunction/activitylifecycle/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("lifecycle","onCreate invoked");
     }
+
+
+//    It is called when the activity becomes visible to the user
     @Override
     protected void onStart() {
         super.onStart();
@@ -27,21 +30,30 @@ public class MainActivity extends AppCompatActivity {
 //        this is phase where user interacts with the app
         Log.d("lifecycle","onResume invoked");
     }
+
+//    It is called when the app is on the background and does not
+//    receive user input
     @Override
     protected void onPause() {
         super.onPause();
         Log.d("lifecycle","onPause invoked");
     }
+//    This phase is called when the activity is no longer visible
+
     @Override
     protected void onStop() {
         super.onStop();
         Log.d("lifecycle","onStop invoked");
     }
+
+//    It is called when the activity restarts after stopping it
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d("lifecycle","onRestart invoked");
     }
+
+//    It is called before the activity is destroyed by the system
     @Override
     protected void onDestroy() {
         super.onDestroy();
