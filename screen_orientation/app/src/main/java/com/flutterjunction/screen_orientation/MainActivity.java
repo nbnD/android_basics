7a6699bf5db7ab96d7c0e9cc5ea27d6dcd7cc9e4
Nabin <ndhakal10@gmail.com>
2023-05-09 21:54:37 +0545

    Initial Commit

---
+++ screen_orientation/app/src/main/java/com/flutterjunction/screen_orientation/MainActivity.java
@@ -0,0 +1,19 @@
+package com.flutterjunction.screen_orientation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void changeOrientation(View view) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
