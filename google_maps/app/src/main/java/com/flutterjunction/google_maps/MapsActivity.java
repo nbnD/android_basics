dd7b61a66fdb019c270b320fe9824323b41d55e2
Nabin <ndhakal10@gmail.com>
2023-05-21 20:57:06 +0545

    Initial Commit

---
+++ google_maps/app/src/main/java/com/flutterjunction/google_maps/MapsActivity.java
@@ -0,0 +1,43 @@
+package com.flutterjunction.google_maps;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.flutterjunction.google_maps.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        float zoomLevel = 16.0f;
+        mMap = googleMap;
+
+        // Add a marker in  and move the camera
+        LatLng position = new LatLng(27.6597881,85.3268811);
+        mMap.addMarker(new MarkerOptions().position(position).title("Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position,zoomLevel));
+    }
+}
