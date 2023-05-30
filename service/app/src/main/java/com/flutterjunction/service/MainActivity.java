35bd079fbf9110fd58e0211497a83d5947bffd89
Nabin <ndhakal10@gmail.com>
2023-05-30 22:37:26 +0545

    Initial Commit

---
+++ service/app/src/main/java/com/flutterjunction/service/MainActivity.java
@@ -0,0 +1,46 @@
+package com.flutterjunction.service;
+
+// MainActivity.java
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    View view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startButton=findViewById(R.id.start_button);
+        Button stopButton=findViewById(R.id.stop_button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMusicService(v);
+                view=v;
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMusicService(v);
+            }
+        });
+
+    }
+
+    public void startMusicService(View view) {
+        Intent serviceIntent = new Intent(this, MusicPlayerService.class);
+        startService(serviceIntent);
+    }
+
+    public void stopMusicService(View view) {
+        Intent serviceIntent = new Intent(this, MusicPlayerService.class);
+        stopService(serviceIntent);
+    }
+}
---
+++ service/app/src/main/java/com/flutterjunction/service/MusicPlayerService.java
@@ -0,0 +1,39 @@
+package com.flutterjunction.service;
+
+// MusicPlayerService.java
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MusicPlayerService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.song); // Load a sample music file
+        mediaPlayer.setLooping(true); // Set looping to true so that the music repeats
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start(); // Start playing the music
+        return START_STICKY; // Indicates that the service should be restarted if it gets terminated
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop(); // Stop the music
+        mediaPlayer.release(); // Release the resources used by the MediaPlayer
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null; // This service does not support binding, so return null
+    }
+}
+
+
